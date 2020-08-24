278b73876d7db7b8f6f3526016acdb4f5de785a5
DenDen047 <sh.mn.nat@gmail.com>
2020-08-24 10:24:00 +0900

    add basic files

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CURRENT_PATH=$(PWD)
+IMAGE_NAME="denden047/jupyerlab"
+
+docker build -t ${IMAGE_NAME} "$CURRENT_PATH"/docker && \
+docker push ${IMAGE_NAME} \
+docker run -it --rm \
+    -p 8890:8888 \
+    -v "$CURRENT_PATH"/notebooks:/home/jovyan \
+    -v "$CURRENT_PATH"/src:/src \
+    ${IMAGE_NAME} \
+    /bin/bash -c "start.sh jupyter lab --NotebookApp.token=''"
