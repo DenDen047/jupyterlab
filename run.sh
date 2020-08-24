3f572b6d8f849f2896313b1fc061a499e589e5fb
DenDen047 <sh.mn.nat@gmail.com>
2020-08-24 10:29:21 +0900

    update Dockerfile

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ CURRENT_PATH=$(PWD)
 IMAGE_NAME="denden047/jupyerlab"
 
 docker build -t ${IMAGE_NAME} "$CURRENT_PATH"/docker && \
-docker push ${IMAGE_NAME} \
 docker run -it --rm \
     -p 8890:8888 \
     -v "$CURRENT_PATH"/notebooks:/home/jovyan \
